cc798cee6e05697a9f8eb099db0079c8fc7cc572
Shuvoghosh7 <shuvoghosh1610@gmail.com>
2022-04-03 11:05:37 +0600

    organic food in route

---
+++ src/Component/Contuct/Contuct.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Contuct = () => {
+    return (
+        <div>
+            <h1>Contuct page</h1>
+        </div>
+    );
+};
+
+export default Contuct;
---
+++ src/Component/Navber/Navber.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../../logo.jpg'
+import CustomLink from '../CustomLink/CustomLink';
+import './Navber.css'
+
+const Navber = () => {
+    return (
+        <div className='nav-bar'>
+            <div className='nav-img'>
+                <img src={logo} alt="" />
+            </div>
+            <div className='nav-link'>
+                <CustomLink to='/'>Home</CustomLink>
+                <CustomLink to='/about'>About</CustomLink>
+                <CustomLink to='/contuct'>Contuct</CustomLink>
+            </div>
+        </div>
+    );
+};
+
+export default Navber;
